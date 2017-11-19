3322f4c01583233e2d0d355a26d5854c1ef392f2
dkitchenerable <kitchenerdavid@gmail.com>
2017-11-19 13:25:35 -0500

    get theater creation working and add theater validation

--- app/services/theater_api_service.rb
+++ app/services/theater_api_service.rb
@@ -7,9 +7,9 @@ class TheaterApiService
     @client = ApiClient.new(options)
   end
 
-  def update_theaters
+  def update_theaters(options= {zip: "10010", radius: 30})
     theater_list = get_theaters
-    create_or_update_list(theater_list)
+    create_or_update_from_list(theater_list)
   end
 
   def get_theaters(options = {zip: "10010", radius: 30})
@@ -21,6 +21,19 @@ class TheaterApiService
   attr_reader :client
 
   def create_or_update_from_list(theater_list)
+    theater_list.each do |t|
+      theater = Theater.find_or_initialize_by(name: t["name"])
+      theater.api_id = t["theatreId"]
+      theater.phone = t["location"]["telephone"]
+      theater.lat = t["location"]["geoCode"]["latitude"]
+      theater.lng = t["location"]["geoCode"]["longitude"]
+      theater.street = t["location"]["address"]["street"]
+      theater.state = t["location"]["address"]["state"]
+      theater.city = t["location"]["address"]["city"]
+      theater.country = t["location"]["address"]["country"]
+      theater.zipcode = t["location"]["address"]["postalCode"]
+      theater.save
+    end
   end
 
 end
