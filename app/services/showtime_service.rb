b3732e40b49a46c0efa0af3365b8b2cc898e7dfc
dkitchener-stash <dkitchener@stashinvest.com>
2019-02-03 11:13:45 -0500

    Revert "add movie theater service; slight refactor"
    This reverts commit 595023243d3881a8aaaca636e18fe29d56b6ae0a.

--- app/services/showtime_service.rb
+++ app/services/showtime_service.rb
@@ -4,24 +4,8 @@ class ShowtimeService
     @data_store = store_class
   end
 
-  def add_showtimes_from_api(api_response, theater)
+  def add_showtimes_from_api(api_response)
     # create movie if necessary
     # add showtimes by theater and date
-    # TODO: add showtimes by movie and date
-  end
-
-  private
-
-  attr_reader :data_store
-
-  def add_by_theater(showtimes)
-    # date = 
-    # theater_id = 
-    # key : key_data
-    data_store.insert("","")
-  end
-
-  def add_by_movie(movie, showtime)
-    data_store.fetch("","")
   end
 end
