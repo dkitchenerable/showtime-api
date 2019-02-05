6069991249a71ef86203cdb1d2e3bab87bffcafd
dkitchener-stash <dkitchener@stashinvest.com>
2019-02-04 21:20:59 -0500

    add actor table, model and tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190204021533) do
+ActiveRecord::Schema.define(version: 20190205015507) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "actors", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "img_url"
+    t.date     "date_of_birth"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
