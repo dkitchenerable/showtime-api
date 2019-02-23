6d5eb02aac6cc5f2e8ab189fec897de0a8c9b1fd
dkitchenerable <kitchener.david@gmail.com>
2019-02-23 15:14:12 -0500

    update and test zipcode validations

--- app/models/zipcode.rb
+++ app/models/zipcode.rb
@@ -2,4 +2,6 @@ class Zipcode < ActiveRecord::Base
   validates_presence_of :code
   validates_presence_of :latitude
   validates_presence_of :longitude
+
+  validates_uniqueness_of :code
 end
--- spec/models/zipcode_spec.rb
+++ spec/models/zipcode_spec.rb
@@ -3,5 +3,9 @@ require 'rails_helper'
 describe Zipcode do
   context "validations" do
     it { should validate_presence_of(:code) }
+    it { should validate_presence_of(:latitude) }
+    it { should validate_presence_of(:longitude) }
+
+    it { should validate_uniqueness_of(:code) }
   end
 end
